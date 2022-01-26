494f9529fc0887cb90dfd8fdb6dbc3ca8d638344
RestoreEmpire <voloshenkoegor23rus@gmail.com>
2022-01-26 17:54:31 +0300

    Database was added

--- src/main/java/com/restoreempire/App.java
+++ src/main/java/com/restoreempire/App.java
@@ -1,36 +1,32 @@
 package com.restoreempire;
 
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-    
-
-        try (Scanner in = new Scanner(System.in)) {
-            while(true){
-            String s = in.nextLine();
-            s = s.strip();
-            switch (s) {
-                case "create":
-                    System.out.println("created");
-                    break;
-                case "read":
-                    System.out.println("readed");
-                    break;
-                case "update":
-                    System.out.println("updated");
-                    break;
-                case "delete":
-                    System.out.println("deleted");
-                    break;
-                default:
-                    break;
-            }
+    public static void main(String[] args)  {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BankApp", 
+        "postgres", 
+        System.getenv("POSTGRES_PASSWORD"))) {
+            PreparedStatement insert = connection.prepareStatement("insert into bank(name) values (?)");
+            PreparedStatement select = connection.prepareStatement("select * from bank where name = ?");
+            insert.setString(1, "Sberbank");
+            insert.executeUpdate();
+            select.setString(1,"Tinkoff");
+            ResultSet result = select.executeQuery();
+            while (result.next()){
+                String name = result.getString("name");
+                System.out.println("Name " + name);
             }
         } catch (Exception e) {
             e.printStackTrace();
-
         }
+
     }
-}   
+
+
+}
